8a59f02a6c832ba58947c36026b56974c80c3210
hadise-shaker <hadise_shaker66@yahoo.com>
2021-05-29 00:34:03 +0430

    menu is completed

--- menu/src/App.js
+++ menu/src/App.js
@@ -1,93 +1,20 @@
-/* import logo from "./logo.svg";
-import "./App.css";
-import { useState } from "react";
-function App() {
-  const defaultTeas = [
-    {
-      name: "Black Tea",
-      time: 180,
-      temp: 100,
-    },
-    {
-      name: "Green Tea",
-      time: 120,
-      temp: 80,
-    },
-    {
-      name: "White Tea",
-      time: 120,
-      temp: 75,
-    },
-  ];
-  const [teatimer, setTea] = useState("");
-  const starttimer = () => {
-    console.log("started");
-    
-  };
-  return (
-    <div className="App">
-      <p>show timer</p>
-      <h1>{teatimer}min left</h1>
-      <br></br>
-      <br></br>
-      <br></br>
-      {defaultTeas.map((tea, index) => {
-        return (
-          <li
-            data-name={tea.name}
-            data-time={tea.time}
-            key={index}
-             onClick={this.setActiveTea}
-            className="tea-box"
-          >
-            <span
-              className="name"
-              onClick={(e) => setTea(tea.time / 60 + ":00")}
-            >
-              {tea.name}
-            </span>
-            <p className="temp">{tea.temp}°C</p>
-            <p className="time">{tea.time}min</p>
-            <span className="note">
-              Brew with {tea.temp}°C water for {tea.time}s.
-            </span>
-          </li>
-        );
-      })}
-      <h2 onClick={starttimer}>start timer</h2>
-    </div>
-  );
-}
-export default App;
- */
-
 import React from "react";
 import "./App.css";
+/* import QuichSelect from "./components/QuichSelect"; */
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      /*  word: 0, */
-      /* second: 0, */
       teaname: "",
       teatime: 0,
+      timerActive: false,
+      timer: null,
     };
-    this.starttimer = this.starttimer.bind(this);
-    this.setActiveTea = this.setActiveTea.bind(this);
-    /*     this.tick = this.tick.bind(this); */
-    /* this.tea = this.tea.bind(this); */
+    this.ActivateTea = this.ActivateTea.bind(this);
+    this.tick = this.tick.bind(this);
+    this.cancelTimer = this.cancelTimer.bind(this);
   }
-
-  tick() {
-    this.setState({
-      teatime: this.state.teatime < 60 ? this.state.teatime - 1 : 0,
-    });
-  }
-  starttimer() {
-    console.log("started");
-    this.interval = setInterval(() => this.tick(), 1000);
-  }
-  defaultTeas = [
+  teaList = [
     {
       name: "Black Tea",
       time: 180,
@@ -104,55 +31,116 @@ class App extends React.Component {
       temp: 75,
     },
   ];
-  settime() {
-    console.log("hi");
-  }
-  setActiveTea(tea) {
-    /*     var tea = event.target.name;
-    var time = event.target.time; */
+  tick(e) {
+    var time = e.target.dataset.time;
+    console.log("started");
+    var count = 0;
+    var self = this;
+    self.setState({
+      timerActive: true,
+    });
 
-    this.setState({ teatime: tea });
+    this.state.timer = setInterval(() => {
+      console.log(time);
+      if (count++ >= time) {
+        clearInterval(self.state.timer);
+        alert("Tea is ready!");
+      } else {
+        var Left = time - count;
+        self.setState({
+          teatime: Left,
+        });
+      }
+    }, 1000);
+  }
+
+  ActivateTea(e) {
+    console.log("activet");
+    var tea = e.target.dataset.name;
+    var time = e.target.dataset.time;
+
+    this.setState({ teaname: tea, teatime: time });
     console.log(this.state.teatime);
   }
-  /*   start() {
-    this.interval = setInterval(() => this.tick(), 1000);
-  } */
+
+  cancelTimer() {
+    clearInterval(this.state.timer);
+    this.setState({
+      teatime: 0,
+    });
+  }
   render() {
+    /* get seconds & change to minute */
+    function changeToMin(seconds) {
+      var secs = Math.round(seconds);
+
+      var minute = secs % (60 * 60);
+      var minutes = Math.floor(minute / 60);
+
+      var second = minute % 60;
+      var seconds = Math.ceil(second);
+      if (seconds.toString().length <= 1) {
+        seconds = +"0" + seconds;
+      }
+      var time = {
+        m: minutes,
+        s: seconds,
+      };
+      return time.m + "." + time.s;
+    }
+    /* **************************** ************************************** */
+    if (this.state.teatime > 0) {
+      var Left = " min left";
+    }
+    var cancletime;
+    if (this.state.timerActive) {
+      cancletime = (
+        <a onClick={this.cancelTimer} className="btn-cancel">
+          Cancel
+        </a>
+      );
+    } else {
+      cancletime = "";
+    }
+
     return (
       <div className="App">
-        <p>show timer</p>
-        <h1>{this.state.teatime}min left</h1>
-        <br></br>
-        <br></br>
-        <br></br>
-        {this.defaultTeas.map((tea, index) => {
-          /*           this.setState({ teaname: tea.name });
-          console.log(this.state.teaname); */
-          return (
-            <li
-              data-name={tea.name}
-              data-time={tea.time}
-              key={index}
-              /*  onClick={this.setActiveTea} */
-              className="tea-box"
+        <section className="tea">
+          <div className="container">
+            <p className="title">{this.state.teaname}</p>
+            <h4 className="output">
+              {changeToMin(this.state.teatime)}
+              {Left}
+            </h4>
+            <ul className="list">
+              {this.teaList.map((tea, index) => (
+                <li
+                  data-name={tea.name}
+                  data-time={tea.time}
+                  key={index}
+                  onClick={this.ActivateTea}
+                  className="tea-box"
+                >
+                  <span className="name">{tea.name}</span>
+                  <p className="temp">{tea.temp}°C</p>
+                  <p className="time">{changeToMin(tea.time)}min</p>
+                  <span className="note">
+                    Brew with {tea.temp}°C water for {tea.time}s.
+                  </span>
+                </li>
+              ))}
+            </ul>
+            <a
+              className="btn"
+              data-time={this.state.teatime}
+              data-name={this.state.teaname}
+              onClick={this.tick}
             >
-              <span
-                className="name"
-                /* onClick={this.setState({ tea.name })} */
-                onClick={() => this.setActiveTea(tea.time)}
-              >
-                {tea.name}
-                {/*  {console.log(this.state.teaname)} */}
-              </span>
-              <p className="temp">{tea.temp}°C</p>
-              <p className="time">{tea.time}min</p>
-              <span className="note">
-                Brew with {tea.temp}°C water for {tea.time}s.
-              </span>
-            </li>
-          );
-        })}
-        <h2 onClick={this.starttimer}>start timer</h2>
+              Start Timer
+            </a>
+            {cancletime}
+          </div>
+        </section>
       </div>
     );
   }
